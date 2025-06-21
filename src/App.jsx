ea6e93d7ffc347b76f0f8f388b7de434847ee6a6
Satya0403 <138397382+Satya0403@users.noreply.github.com>
2025-06-21 22:54:39 +0530

    image added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import Navbar from './components/Navbar/Navbar'
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
-    <div className='bg-gray-900 min-h-screen'>
+    <div className="bg-gray-900 min-h-screen">
       <Navbar></Navbar>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
