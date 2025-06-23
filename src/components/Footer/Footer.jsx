732108eb2b6bb80b46e3546e5d72a22f68ead9e6
Sita Ram Sahu <103084791+sitaramsahu@users.noreply.github.com>
2025-06-23 08:10:55 +0530

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,45 +6,43 @@ const Footer = () => {
     <>
       <div>
         <div className="flex flex-col items-center font-bold bg-slate-200 ">
-          <ul className="flex space-x-8 justify-center p-3">
-            <li>
-              {" "}
-              <a href="#">
-                {" "}
-                <Instagram />{" "}
-              </a>{" "}
-            </li>
-            <li>
-              {" "}
-              <a href="#">
-                {" "}
-                <Facebook />{" "}
-              </a>{" "}
-            </li>
-            <li>
-              {" "}
-              <a href="#">
-                {" "}
-                <Twitter />{" "}
-              </a>{" "}
-            </li>
-            <li>
-              {" "}
-              <a href="#">
-                {" "}
-                <Linkedin />{" "}
-              </a>{" "}
-            </li>
-            <li>
-              {" "}
-              <a href="#">
-                {" "}
-                <Youtube />{" "}
-              </a>{" "}
-            </li>
-
-            <p>Copyright © SitaRamSahu. All rights reserved.</p>
-            <div className="text-center">
+          <ul className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-8 justify-center items-center p-3">
+            <div className="flex gap-8 items-center justify-center">
+              <li>
+                <a href="#">
+                  <Instagram />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <Facebook />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <Youtube />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <Twitter />
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <Linkedin />
+                </a>
+              </li>
+            </div>
+            <p className="hidden md:block mt-2 md:mt-0">
+              Copyright &copy; {new Date().getFullYear()} Geeta Medicals. All
+              rights reserved.
+            </p>
+            <div className="text-center ">
+              <p className="block md:hidden mt-2 md:mt-0">
+                Copyright &copy; {new Date().getFullYear()} Geeta Medicals. All
+                rights reserved.
+              </p>
               <img src="https://asrsnews.in/wp-content/uploads/2025/02/payments1.png" />
             </div>
           </ul>
